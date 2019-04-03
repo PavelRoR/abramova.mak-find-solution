b043de42623f491858f555668a28ec1f813de28c
pavelp21 <pavelvp2182@yandex.ru>
2019-04-04 00:14:10 +0500

    Main sction is done

--- js/_js/scripts.js
+++ js/_js/scripts.js
@@ -1,5 +1,9 @@
-$(document).ready(function() {
+//@prepros-prepend jquery-2.1.1.min.js
+//@prepros-prepend bootstrap.min.js
+//@prepros-prepend scroller.min.js
 
+$(document).ready(function() {
+    $("[data-paroller-factor]").paroller();
 
 
 /*Конец документа*/

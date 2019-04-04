02b51ed22c989aa10cbc7c87897ebfae04d1ce4b
pavelp21 <pavelvp2182@yandex.ru>
2019-04-04 15:42:29 +0500

    Section Get is done

--- js/_js/scripts.js
+++ js/_js/scripts.js
@@ -4,7 +4,47 @@
 
 $(document).ready(function() {
     $("[data-paroller-factor]").paroller();
-
+   /* Работа формы */
+   $(function () {
+    var check = $('.check', this),
+        email = $('.mail', this),
+        message = $('.alert_message', this),
+        button = $('.button_mail', this);
+    $(".form").on("submit", function () {
+        var check = $('.check', this),
+            message = $('.alert_message', this),
+            reNone = /.+/;
+        if (!email.val().match(reNone)) {
+            email.css('borderColor', 'red');
+            // message.text('Введите email').slideDown(500);
+            button.text('Введите email');
+            return false;
+        }
+        if (!check.prop("checked")) {
+            check.next().css("color", "#000");
+            check.next().children().css("color", "#000");
+            message.text('Подтвердите соглашение').slideDown(500);
+            return false;
+        }
+        if (email.val() && check) {
+            // button.text('Отправляем...');
+            button.text('Отправляем...');
+            setTimeout(function () {
+                button.text('Отправлено!');
+            }, 500);
+        }
+    });
+    email.click(function () {
+        email.css("borderColor", "rgba(80,61,37,.1)");
+        // message.slideUp(500);
+        button.text('Участвовать бесплатно');
+    });
+    check.click(function () {
+        check.next().css("color", "rgba(255,255,255, .9)");
+        check.next().children().css("color", "rgba(255,255,255, .9)");
+        message.slideUp(500);
+    });
+});
 
 /*Конец документа*/
 });
